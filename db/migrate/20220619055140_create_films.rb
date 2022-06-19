5cea5348fa6edc7694a042785ee48006b19d0db9
max pleaner <maxpleaner@gmail.com>
2022-06-18 23:09:43 -0700

    add film description

--- db/migrate/20220619055140_create_films.rb
+++ db/migrate/20220619055140_create_films.rb
@@ -3,6 +3,7 @@ class CreateFilms < ActiveRecord::Migration[7.0]
     create_table :films do |t|
       t.string :name
       t.integer :year
+      t.text :description
       t.string :director
 
       t.timestamps
